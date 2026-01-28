b2339bb3bad8bb9fd631d2888e2c3e7e4511be65
Azzi <locked.hrt@hotmail.fr>
2026-01-28 11:58:34 +0100

    feature home

--- Assur_Aimant/assur_predict/views.py
+++ Assur_Aimant/assur_predict/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
 import joblib
 from django.conf import settings
 from pathlib import Path
 
-MODEL_PATH = Path(settings.BASE_DIR) / "prediction" / "ml" / "gb_pipeline.joblib"
-RMSE_PATH = Path(settings.BASE_DIR) / "prediction" / "ml" / "rmse.joblib"
+# Chemin absolu vers le modèle ML
+MODEL_PATH = Path(settings.BASE_DIR) / "assur_predict" / "ml" / "gb_pipeline.joblib"
+RMSE_PATH = Path(settings.BASE_DIR) / "assur_predict" / "ml" / "rmse.joblib"
 
+# Charger les modèles
 model = joblib.load(MODEL_PATH)
 rmse = joblib.load(RMSE_PATH)
+
+def home(request):
+    return render(request, 'assur_predict/home.html')
