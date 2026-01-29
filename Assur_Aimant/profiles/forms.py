77c856ac1afe27d3403b07544c588e3a0f016124
Edilene <eassuncaosilva@gmail.com>
2026-01-30 00:06:45 +0100

    update: profile prediction fixed

--- Assur_Aimant/profiles/forms.py
+++ Assur_Aimant/profiles/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 from .models import Profile
 
+
+SEX_CHOICES = [
+    ("male", "Male"),
+    ("female", "Female"),
+]
+
+REGION_CHOICES = [
+    ("northwest", "Northwest"),
+    ("northeast", "Northeast"),
+    ("southwest", "Southwest"),
+    ("southeast", "Southeast"),
+]
+
+SMOKER_CHOICES = [
+    (True, "Oui"),
+    (False, "Non"),
+]
+
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
@@ -17,9 +36,23 @@ class ProfileForm(forms.ModelForm):
 
         widgets = {
             'age': forms.NumberInput(attrs={'class': 'form-control'}),
-            'sex': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'sex': forms.Select(
+                choices=SEX_CHOICES,
+                attrs={'class': 'form-control'}
+            ),
+
             'bmi': forms.NumberInput(attrs={'class': 'form-control'}),
+
             'children': forms.NumberInput(attrs={'class': 'form-control'}),
-            'smoker': forms.CheckboxInput(),
-            'region': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'smoker': forms.Select(
+                choices=SMOKER_CHOICES,
+                attrs={'class': 'form-control'}
+            ),
+
+            'region': forms.Select(
+                choices=REGION_CHOICES,
+                attrs={'class': 'form-control'}
+            ),
         }
