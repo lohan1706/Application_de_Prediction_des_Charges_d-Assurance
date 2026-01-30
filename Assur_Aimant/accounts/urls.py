db0b041dc4a6bf20c45c1d29da1b91b051ab2caa
lohan arnaut <lohan.arnaut17@gmail.com>
2026-01-30 12:29:21 +0100

    fix: log, logout, profile

--- Assur_Aimant/accounts/urls.py
+++ Assur_Aimant/accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import SignupView, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LogoutView
+from profiles.views import ProfileView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+
 ]
