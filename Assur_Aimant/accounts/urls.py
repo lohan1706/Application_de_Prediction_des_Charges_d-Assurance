1a57a0cebe93694c947ed0a011392e2d492268e4
Edilene <eassuncaosilva@gmail.com>
2026-01-30 12:34:21 +0100

    Update: URLs and templates changed, addition of tailwind confi

--- Assur_Aimant/accounts/urls.py
+++ Assur_Aimant/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import SignupView, CustomLoginView, CustomLogoutView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
---
+++ Assur_Aimant/theme/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "../../templates/**/*.html",
+    "../templates/**/*.html",
+    "../../**/templates/**/*.html",
+  ],
+  theme: {
+    extend: {
+  colors: {
+    orangeSoft: "#FA9078",
+    ivory: "#FFFFF4",
+    pastelGreen: "#8BD593",
+    grayGreen: "#516060",
+    } 
+}
+,
+  },
+  plugins: [],
+}
