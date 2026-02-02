9c2be60187c4a280c776fbe865f06a6d9045a376
Azzi <locked.hrt@hotmail.fr>
2026-02-02 11:50:03 +0100

    feature creation app prediction

--- Assur_Aimant/profiles/urls.py
+++ Assur_Aimant/profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, PredictionView
 
 urlpatterns = [
-   path("profile/", ProfileView.as_view(), name="profile"),
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('prediction/', PredictionView.as_view(), name='prediction'),
 ]
