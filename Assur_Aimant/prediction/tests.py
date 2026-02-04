1690df5050a258f95f16e0e6a11fb1a75dbebd03
Edilene <eassuncaosilva@gmail.com>
2026-02-04 11:20:51 +0100

    Tests: Unit tests in prediction and chnages related to it

--- Assur_Aimant/prediction/tests.py
+++ Assur_Aimant/prediction/tests.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from .models import Prediction
+from profiles.models import Profile
 
 CustomUser = get_user_model()
 
 class PredictionModelsTest(TestCase):
-    #Tests pour les modèles
+    """Tests pour les modèles"""
 
     def setUp(self):
         #création d'utilisateur et prédiction pour faire les tests
@@ -49,3 +50,69 @@ class PredictionModelsTest(TestCase):
         prediction_id= self.prediction.id
         self.user.delete()
         self.assertFalse(Prediction.objects.filter(id=prediction_id).exists())
+    
+    def test_prediction_related_name(self):
+        #test pour voir si 'predictions' fcontionne (lien prediction - user)
+        predictions = self.user.predictions.all()
+        self.assertEqual(predictions.count(),1)
+        self.assertEqual(predictions.first(), self.prediction)
+
+class PredictView(TestCase):
+# """Tests pour les vues""" 
+    
+    def setUp(self):
+    #creer un utilisateur et un profil
+        self.user=CustomUser.objects.create_user(
+            username='testuser',
+            password='password123'
+        )
+
+        self.profile = Profile.objects.create(
+            user = self.user,
+            age=30,
+            sex='male',
+            bmi=25.5,
+            children=2,
+            smoker=False,
+            region='northwest'
+        )
+        self.predict_url = reverse('predict')
+    
+    def test_view_redirect_if_not_logged_in(self):
+        #test redirecttion vers login si non authentifié
+        response = self.client.get(self.predict_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/login/', response.url)
+    
+    def test_view_accessible_if_logged_in(self):
+       # vue accessible si client authentifié
+       self.client.login(username='testuser', password='password123')
+       response = self.client.get(self.predict_url)
+       self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+       #tester si vue utilise le bon template
+       self.client.login(username='testuser', password='password123')
+       response = self.client.get(self.predict_url)
+       self.assertTemplateUsed(response, 'prediction/predict.html')
+    
+    def test_view_shows_profile_data(self):
+        # tester l'affichage des données du profil
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(self.predict_url)
+        
+        self.assertContains(response, '30')  # Age
+        self.assertContains(response, '25.5')  # BMI
+        self.assertContains(response, '2')  # Children
+
+    # def test_view_without_profile(self):
+    #     #Test : vue redirige vers profil si pas de profil
+    #     user_no_profile = CustomUser.objects.create_user(
+    #         username='noprofile',
+    #         password='password123'
+    #     )
+    #     self.client.login(username='noprofile', password='testpass123')
+    #     response = self.client.get(self.predict_url)
+        
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, 'Compléter mon profil')
