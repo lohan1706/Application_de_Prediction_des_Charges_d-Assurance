42678938573a22fa63ae7a06752675d9b5aa5231
lohan arnaut <lohan.arnaut17@gmail.com>
2026-02-04 15:24:35 +0100

    feat: height wheight

--- Assur_Aimant/profiles/models.py
+++ Assur_Aimant/profiles/models.py
@@ -19,14 +19,19 @@ class CustomUser(AbstractUser):
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
-    age = models.IntegerField(
-        validators=[MinValueValidator(18)]
-    )
+    age = models.IntegerField(validators=[MinValueValidator(18)])
     sex = models.CharField(max_length=10)
-    bmi = models.FloatField()
+    height = models.FloatField(help_text="Taille en mètres")  # ajout de la taille
+    weight = models.FloatField(help_text="Poids en kg")       # ajout du poids
+    bmi = models.FloatField(blank=True)                        # IMC calculé automatiquement
     children = models.IntegerField(validators=[MinValueValidator(0)])
     smoker = models.BooleanField()
     region = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        if self.height > 0 and self.weight > 0:
+            self.bmi = round(self.weight / (self.height ** 2), 1)  # calcul automatique
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
